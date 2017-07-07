53c901f9ea991b06dd91f349bc586b2c3752e082
rohansuri <rohansuri1996@gmail.com>
2017-07-07 15:09:29 +0530

    Fabric-crashlytics added to the app successfully.

--- app/src/main/java/com/equ/rohansuri/entrepreneurquotebook/SplashScreen.java
+++ app/src/main/java/com/equ/rohansuri/entrepreneurquotebook/SplashScreen.java
@@ -3,12 +3,15 @@ package com.equ.rohansuri.entrepreneurquotebook;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 
 public class SplashScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
